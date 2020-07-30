942dcbca310ef735df88b9db420915f1d31b21d3
dora-ops <575703414@qq.com>
2020-07-31 00:33:31 +0800

    界面化操作

---
+++ public/core.js
@@ -0,0 +1,55 @@
+//自动识别切割
+function dateFormat(fmt, date) {
+    let ret;
+    const opt = {
+        "Y+": date.getFullYear().toString(),        // 年
+        "m+": (date.getMonth() + 1).toString(),     // 月
+        "d+": date.getDate().toString(),            // 日
+        "H+": date.getHours().toString(),           // 时
+        "M+": date.getMinutes().toString(),         // 分
+        "S+": date.getSeconds().toString()          // 秒
+        // 有其他格式化字符需求可以继续添加，必须转化成字符串
+    };
+    for (let k in opt) {
+        ret = new RegExp("(" + k + ")").exec(fmt);
+        if (ret) {
+            fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+        };
+    };
+    return fmt;
+}
+
+/**
+* 将数值四舍五入(保留2位小数)后格式化成金额形式
+*
+* @param num 数值(Number或者String)
+* @return 金额格式的字符串,如'1,234,567.45'
+* @type String
+*/
+function formatCurrency(num) {
+    num = num.toString().replace(/\$|\,/g, '');
+    if (isNaN(num))
+        num = "0";
+    sign = (num == (num = Math.abs(num)));
+    num = Math.floor(num * 100 + 0.50000000001);
+    cents = num % 100;
+    num = Math.floor(num / 100).toString();
+    if (cents < 10)
+        cents = "0" + cents;
+    for (var i = 0; i < Math.floor((num.length - (1 + i)) / 3); i++)
+        num = num.substring(0, num.length - (4 * i + 3)) + ',' +
+            num.substring(num.length - (4 * i + 3));
+    return (((sign) ? '' : '-') + num + '.' + cents);
+}
+//随机打乱
+function shuffle(a) {
+    var len = a.length;
+    for (var i = 0; i < len; i++) {
+        var end = len - 1;
+        var index = (Math.random() * (end + 1)) >> 0;
+        var t = a[end];
+        a[end] = a[index];
+        a[index] = t;
+    }
+    return a;
+};
