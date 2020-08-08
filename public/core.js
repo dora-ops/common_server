7256d243bf46a2695a7fe0b35f016cac4f8d8cfe
dora-ops <575703414@qq.com>
2020-08-08 18:17:14 +0800

    余额宝

--- public/core.js
+++ public/core.js
@@ -19,6 +19,25 @@ function dateFormat(fmt, date) {
     return fmt;
 }
 
+//制保留2位小数，如：2，会在2后面补上00.即2.00 
+ function toDecimal2(x) {
+    var f = parseFloat(x);
+    if (isNaN(f)) {
+        return false;
+    }
+    var f = Math.round(x * 100) / 100;
+    var s = f.toString();
+    var rs = s.indexOf('.');
+    if (rs < 0) {
+        rs = s.length;
+        s += '.';
+    }
+    while (s.length <= rs + 2) {
+        s += '0';
+    }
+    return s;
+}
+
 /**
 * 将数值四舍五入(保留2位小数)后格式化成金额形式
 *
