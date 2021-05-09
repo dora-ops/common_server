7a855cb574cba6c2109e79e6301067325829f37c
dora-ops <575703414@qq.com>
2021-05-09 14:01:45 +0800

    1

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
     mysql: {
       // host: '192.168.0.105',
-      // host: '127.0.0.1',  // 新建数据库连接时的 主机名或ID地址 内容
-      host:'114.55.142.72',
+      host: '127.0.0.1',  // 新建数据库连接时的 主机名或ID地址 内容
+    //   host:'114.55.142.72',
       user: 'root', 
-      password: '#$ERDFCV5tgb', // root 密码
-      database: 'graduate', // 数据库名
+      password: 'root', // root 密码
+      database: 'illustrator', // 数据库名
       port: '3306'
     },
     port:'3000'
