eb0036e7a29dcce9901d77e55d45a003e492468e
dora-ops <575703414@qq.com>
2021-05-18 20:37:54 +0800

    1

--- api/baseApi.js
+++ api/baseApi.js
@@ -28,13 +28,13 @@ router.all('*', function (req, res, next) {
 
     }
     res.header("Origin", "*");
-    res.header("Access-Control-Allow-Credentials", "true")
+    // res.header("Access-Control-Allow-Credentials", "true")
     res.header("Access-Control-Allow-Origin", "*");
     // res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     // res.header("X-Powered-By",' 3.2.1')
     // res.header("Content-Type", "application/json;charset=utf-8");
-    res.header("Access-Control-Allow-Headers", "Content-Type,application/x-www-form-urlencoded");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
     // res.setHeader("Access-Control-Allow-Headers", "x-requested-with,application/x-www-form-urlencoded");
     next();
 });
